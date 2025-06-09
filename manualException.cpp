7c9a4c27bf525a68dd203c389ded6bca88fc2847
Ridhanovita <novitaridha796@gmail.com>
2025-06-09 11:24:49 +0700

    blok catch dan akhir program

--- manualException.cpp
+++ manualException.cpp
@@ -8,5 +8,14 @@ int main()
         throw 0.5; // melemparkan sebuah integer maka
         cout << "Pernyataan tidak akan dieksekusi" << endl;
     }
-    
+    catch (int a) {
+        // blok ini akan dieksekusi
+        cout << "Pengecualian akan diesekusi" << endl;
+    }
+    catch (...) {
+        /*jika selain integer maka blok ini akan dieksekusi*/
+        cout << "default pengecualian eksekusi" << endl;
+    }
+
+    return 0;
 }
