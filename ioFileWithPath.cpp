ae3eb6feade4b9975e1203efc376c7cdd0a5044c
Ridhanovita <novitaridha796@gmail.com>
2025-06-09 11:22:18 +0700

    menulis ke file

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -10,5 +10,25 @@ int main() {
     cout << "Masukkan nama file: ";
     cin >> namaFile;
 
+    // oprasi file dalam mode menulis.
+    ofstream outfile;
+    // menunjuk ke sebuah nama file untuk membuka
+    outfile.open(namaFile + ".txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    // unlimited loop untuk menullis
+    while(true) {
+        cout << "- ";
+        // mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        // loop akan berhenti jika anda memasukan karakter q
+        if(baris == "q") break;
+        // menulis dan memasukan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+    }
+    // selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+
    
 }
