c4a9e00c87db15e006f0575f4da0d201f9ff571d
Ridhanovita <novitaridha796@gmail.com>
2025-06-09 11:28:09 +0700

    blok try

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -8,5 +8,11 @@ using namespace std;
 int main()
 {
     cout << "Awal Program" << endl; // penanda I:...
-
+    try {
+        array<int, 3> data = {1, 2, 3};
+        // pesan array integer 3 elemen
+        cout<<data.at(5)<<endl;
+        // memanggil array elemen ke 5
+    }
+    
 }
